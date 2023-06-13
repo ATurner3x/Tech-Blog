12ff8dbf20c7c9f037d51c1082a1689340234b73
Amadeus <a.turner3x@gmail.com>
2023-06-13 19:15:57 -0400

    created server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+// Required packages
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
+app.set("view engine", "handlebars");
+
+
