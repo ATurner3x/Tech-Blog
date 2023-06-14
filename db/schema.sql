25d20af114c16d4607df46b964aee9a8de2d98d6
Amadeus <a.turner3x@gmail.com>
2023-06-14 05:45:32 -0400

    created the users table

--- db/schema.sql
+++ db/schema.sql
@@ -6,3 +6,15 @@ CREATE DATABASE techblog_db;
 
 -- Switch to the new database
 USE techblog_db;
+
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+
+  // Timestamps
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
